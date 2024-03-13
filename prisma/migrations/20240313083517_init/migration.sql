02ebde6f5eac66325b97611f75bdf48dfccb9d83
HACK-WITH-JS <jungmini2040@gmail.com>
2024-03-13 19:08:39 +0900

    프리즈마 모듈 추가 및 유저 스키마 providerId 필드 추가

--- prisma/migrations/20240313083517_init/migration.sql
+++ prisma/migrations/20240313083517_init/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE `User` (
     `nickName` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NULL,
     `profileImageUrl` VARCHAR(191) NULL,
+    `providerId` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `positionId` INTEGER NOT NULL,
     `positionOpenStatus` ENUM('OPEN', 'CLOSE') NOT NULL DEFAULT 'CLOSE',
