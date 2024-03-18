7120d6a9252495a199f146456d60f278e6cc74e5
HACK-WITH-JS <jungmini2040@gmail.com>
2024-03-18 11:50:39 +0900

    헬스 체크 기능 추가

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { PrismaIndecator } from './prisma.indicator';
 
 @Global()
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService, PrismaIndecator],
+  exports: [PrismaService, PrismaIndecator],
 })
 export class PrismaModule {}
